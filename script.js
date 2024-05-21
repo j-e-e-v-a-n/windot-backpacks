d53c4f254bbc375611e933392369676e10f6e797
Jeevan Joy <103121525+J-e-e-v-a-n@users.noreply.github.com>
2024-05-21 16:49:31 +0530

    .

--- script.js
+++ script.js
@@ -1,55 +1,163 @@
+let listProductHTML = document.querySelector('.listProduct');
+let listCartHTML = document.querySelector('.listCart');
+let iconCart = document.querySelector('.icon-cart');
+let iconCartSpan = document.querySelector('.icon-cart span');
+let body = document.querySelector('body');
+let closeCart = document.querySelector('.close');
+let checkOutButton = document.querySelector('.checkOut');
+let products = [];
+let cart = [];
 
-            document.addEventListener("DOMContentLoaded", function () {
-                // Temporary bag data
-                const bags = [
-                    {
-                        name: "Stylish Backpack",
-                        imageUrl: "images/bag1.jpg",
-                        description: "A trendy backpack for everyday use."
-                    },
-                    {
-                        name: "Leather Messenger Bag",
-                        imageUrl: "images/bag2.jpg",
-                        description: "Classic leather messenger bag, perfect for work or travel."
-                    },
-                    {
-                        name: "Canvas Tote Bag",
-                        imageUrl: "images/bag3.jpg",
-                        description: "Casual canvas tote bag for shopping or beach outings."
-                    },
-                    {
-                        name: "Canvas Tote Bag",
-                        imageUrl: "images/bag4.jpg",
-                        description: "Casual canvas tote bag for shopping or beach outings."
-                    },
-                    {
-                        name: "Canvas Tote Bag",
-                        imageUrl: "images/bag5.jpg",
-                        description: "Casual canvas tote bag for shopping or beach outings."
-                    },
-                ];
+iconCart.addEventListener('click', () => {
+    body.classList.toggle('showCart');
+});
+closeCart.addEventListener('click', () => {
+    body.classList.toggle('showCart');
+});
 
-                const bagContainer = document.getElementById("bagContainer");
+const addDataToHTML = () => {
+    // remove default data from HTML
+    listProductHTML.innerHTML = '';
+    
+    // add new data
+    if(products.length > 0) {
+        products.forEach(product => {
+            let newProduct = document.createElement('div');
+            newProduct.dataset.id = product.id;
+            newProduct.classList.add('item');
+            newProduct.innerHTML = 
+            `<img src="${product.image}" alt="">
+            <h2>${product.name}</h2>
+            <div class="price">$${product.price}</div>
+            <button class="addCart">Add To Cart</button>`;
+            listProductHTML.appendChild(newProduct);
+        });
+    }
+};
+listProductHTML.addEventListener('click', (event) => {
+    let positionClick = event.target;
+    if(positionClick.classList.contains('addCart')) {
+        let id_product = positionClick.parentElement.dataset.id;
+        addToCart(id_product);
+    }
+});
+const addToCart = (product_id) => {
+    let positionThisProductInCart = cart.findIndex((value) => value.product_id == product_id);
+    if(cart.length <= 0) {
+        cart = [{
+            product_id: product_id,
+            quantity: 1
+        }];
+    } else if(positionThisProductInCart < 0) {
+        cart.push({
+            product_id: product_id,
+            quantity: 1
+        });
+    } else {
+        cart[positionThisProductInCart].quantity = cart[positionThisProductInCart].quantity + 1;
+    }
+    addCartToHTML();
+    addCartToMemory();
+};
+const addCartToMemory = () => {
+    localStorage.setItem('cart', JSON.stringify(cart));
+};
+const addCartToHTML = () => {
+    listCartHTML.innerHTML = '';
+    let totalQuantity = 0;
+    if(cart.length > 0) {
+        cart.forEach(item => {
+            totalQuantity = totalQuantity + item.quantity;
+            let newItem = document.createElement('div');
+            newItem.classList.add('item');
+            newItem.dataset.id = item.product_id;
 
-                // Loop through bag data and create HTML elements
-                bags.forEach(bag => {
-                    const bagElement = document.createElement("div");
-                    bagElement.classList.add("bag");
+            let positionProduct = products.findIndex((value) => value.id == item.product_id);
+            let info = products[positionProduct];
+            listCartHTML.appendChild(newItem);
+            newItem.innerHTML = `
+            <div class="image">
+                <img src="${info.image}">
+            </div>
+            <div class="name">
+                ${info.name}
+            </div>
+            <div class="totalPrice">$${info.price * item.quantity}</div>
+            <div class="quantity">
+                <span class="minus"><</span>
+                <span>${item.quantity}</span>
+                <span class="plus">></span>
+            </div>
+            `;
+        });
+    }
+    iconCartSpan.innerText = totalQuantity;
+};
+listCartHTML.addEventListener('click', (event) => {
+    let positionClick = event.target;
+    if(positionClick.classList.contains('minus') || positionClick.classList.contains('plus')) {
+        let product_id = positionClick.parentElement.parentElement.dataset.id;
+        let type = 'minus';
+        if(positionClick.classList.contains('plus')) {
+            type = 'plus';
+        }
+        changeQuantityCart(product_id, type);
+    }
+});
+const changeQuantityCart = (product_id, type) => {
+    let positionItemInCart = cart.findIndex((value) => value.product_id == product_id);
+    if(positionItemInCart >= 0) {
+        let info = cart[positionItemInCart];
+        switch(type) {
+            case 'plus':
+                cart[positionItemInCart].quantity = cart[positionItemInCart].quantity + 1;
+                break;
+            default:
+                let changeQuantity = cart[positionItemInCart].quantity - 1;
+                if(changeQuantity > 0) {
+                    cart[positionItemInCart].quantity = changeQuantity;
+                } else {
+                    cart.splice(positionItemInCart, 1);
+                }
+                break;
+        }
+    }
+    addCartToHTML();
+    addCartToMemory();
+};
 
-                    const imgElement = document.createElement("img");
-                    imgElement.src = bag.imageUrl;
-                    imgElement.alt = bag.name;
+const generateWhatsAppLink = () => {
+    let message = "I would like to know more about the following products:\n";
 
-                    const h2Element = document.createElement("h2");
-                    h2Element.textContent = bag.name;
+    cart.forEach(item => {
+        let product = products.find(p => p.id == item.product_id);
+        if (product) {
+            message += `${product.name} - ${item.quantity}\n`;
+        }
+    });
 
-                    const pElement = document.createElement("p");
-                    pElement.textContent = bag.description;
+    let phoneNumber = '8113064544';
+    let encodedMessage = encodeURIComponent(message);
+    let link = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+    window.open(link, '_blank');
+};
 
-                    bagElement.appendChild(imgElement);
-                    bagElement.appendChild(h2Element);
-                    bagElement.appendChild(pElement);
 
-                    bagContainer.appendChild(bagElement);
-                });
-            });
+checkOutButton.addEventListener('click', generateWhatsAppLink);
+
+const initApp = () => {
+    // get data product
+    fetch('products.json')
+    .then(response => response.json())
+    .then(data => {
+        products = data;
+        addDataToHTML();
+
+        // get data cart from memory
+        if(localStorage.getItem('cart')) {
+            cart = JSON.parse(localStorage.getItem('cart'));
+            addCartToHTML();
+        }
+    });
+};
+initApp();
